9ce75877e03c7b158f6b762b3d5c0ecbe002d929
Carlos Alvarez del Castillo Garcia <psykher@RZR>
2022-05-18 06:43:53 -0500

    Update sample

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import '../assets/css/style.css';
+
+console.log('Hola Mundo!');
+
 /*
     ===== Código de TypeScript =====
 */
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.ts',
@@ -9,14 +10,24 @@ module.exports = {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/
-            }
+            },
+            {
+                test: /\.(css)$/,
+                use: ['style-loader', 'css-loader'],
+            },
         ]
     },
     resolve: {
-        extensions: [ '.tsx', '.ts', '.js' ]
+        extensions: ['.tsx', '.ts', '.js']
     },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            favicon: './assets/img/favicon.png'
+        })
+    ]
 };
